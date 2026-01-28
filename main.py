8a83c30e73788036d4923706a688c927cd1d3ce0
Surfs-Up5 <cgparsons5@gmail.com>
2026-01-28 13:01:17 -0500

    fixed typo

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ from cryptography.hazmat.primitives.asymmetric import ed25519
 private_key = ed25519.Ed25519PrivateKey.generate()
 
 #Get corresponding public key
-pubic_key = private_key.public_key()
+public_key = private_key.public_key()
+
+#printing keys for learning
+public_bytes = public_key.public_bytes(
+    encoding=serialization.Encoding.Raw,
+    format=serialization.PublicFormat.Raw
+)
