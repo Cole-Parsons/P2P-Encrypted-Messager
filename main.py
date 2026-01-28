d931c1aecfaf1555d7965d6d1b96b0d5cfc603e0
Surfs-Up5 <cgparsons5@gmail.com>
2026-01-28 12:53:24 -0500

    created private and public key

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 # Key exchange: X25519
 # Message encryption: ChaCha20-Poly1305 (better for software)
 # Hashing and operating system secure rnd num: SHA-256
+
+#Create user Identity, A long term cryptographic key pair that proves "this is me"
+#   -Need private and public Ed25519 key
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
+#Generate Private key
+private_key = ed25519.Ed25519PrivateKey.generate()
+
+#Get corresponding public key
+pubic_key = private_key.public_key()
