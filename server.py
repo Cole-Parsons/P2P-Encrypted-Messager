e5412ecd59d1dc5848922c607641f31ddb1e9d7d
Surfs-Up5 <cgparsons5@gmail.com>
2026-01-30 14:16:57 -0500

    added multi threading

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
 #TODO: add multithreading to handle multiple clients
 
 import socket
-
 import datetime
+import threading
+import time
+
+def allow_client():
+    client, addr = server.accept()
+    print(f'connected form {addr}')
+
+    while True:
+        data = client.recv(1024)
+
+        if not data:
+            print('client disconnected')
+            break
+        timestamp = datetime.datetime.now()
+        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        msg = data.decode()    
+        print(f'[{timestamp_str}] client: {msg}')
+        client.send('message recieved'.encode())
+
+    client.close()
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,25 +33,24 @@ server.listen(2)
 print('Server listening on port 8080...')
 
 #what server is doing
-while True:
 
-    client, addr = server.accept()
-    print(f'connected from {addr}')
+t1 = threading.Thread(target=allow_client, daemon=True)
+t2 = threading.Thread(target=allow_client, daemon=True)
 
-    while True:
-        data = client.recv(1024)
+t1.start()
+t2.start()
 
-        if not data:
-            print('client disconnected')
-            break
+end = input('press enter to shut down server: ')
 
-        timestamp = datetime.datetime.now()
-        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        msg = data.decode()
-        print(f'[{timestamp_str}] client: {msg}')
 
-        client.send('message recieved'.encode())
 
-    client.close()
+        
+        
+        
+        
+
+        
+
+    
 
 
