c31135ed5654e3f25075f6f382bd7364c8e87c61
Surfs-Up5 <cgparsons5@gmail.com>
2026-01-31 16:53:34 -0500

    on client connection, server adds client info to dictionary

--- server.py
+++ server.py
@@ -1,25 +1,47 @@
-#TODO: add multithreading to handle multiple clients
+
 
 import socket
 import datetime
 import threading
 import time
 
-def allow_client():
+connected_clients = {'clients': []}
+clients_lock = threading.Lock()
+id_counter = 0
+
+
+def allow_client(connected_clients):
     client, addr = server.accept()
-    print(f'connected form {addr}')
+    ip, port = addr
+
+    global id_counter
+    id_counter += 1
+
+    print(f'\nclient{id_counter} connected form {addr}')
+
+    with clients_lock:
+        connected_clients[f'client{id_counter}'] = {
+            'ip': ip,
+            'port': port,
+        }
+
+    print(connected_clients)
 
     while True:
-        data = client.recv(1024)
+        try:
+            data = client.recv(1024)
 
-        if not data:
-            print('client disconnected')
+            if not data:
+                print('client disconnected')
+                break
+            timestamp = datetime.datetime.now()
+            timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+            msg = data.decode()    
+            print(f'[{timestamp_str}] {addr}: {msg}')
+            client.send('message recieved'.encode())
+        except:
             break
-        timestamp = datetime.datetime.now()
-        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        msg = data.decode()    
-        print(f'[{timestamp_str}] client: {msg}')
-        client.send('message recieved'.encode())
 
     client.close()
 
@@ -34,23 +56,10 @@ print('Server listening on port 8080...')
 
 #what server is doing
 
-t1 = threading.Thread(target=allow_client, daemon=True)
-t2 = threading.Thread(target=allow_client, daemon=True)
+t1 = threading.Thread(target=allow_client, args=(connected_clients,), daemon=True)
+t2 = threading.Thread(target=allow_client, args=(connected_clients,), daemon=True)
 
 t1.start()
 t2.start()
 
-end = input('press enter to shut down server: ')
-
-
-
-        
-        
-        
-        
-
-        
-
-    
-
-
+end = input('press enter to shut down server: \n')
